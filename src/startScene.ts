ac60d334bd874d09f743c9c8714bce4da0f575a1
vogellen30 <vogellen30@gmail.com>
2023-12-14 16:47:38 +0100

    laatste update, Level moet toegevoegd worden

--- src/startScene.ts
+++ src/startScene.ts
@@ -1,16 +1,17 @@
 import CanvasRenderer from './CanvasRenderer.js';
 import MouseListener from './MouseListener.js';
 import Scene from './Scene.js';
+// import Level from './Level.js';
 
 export default class SceneStart extends Scene {
     private starting: boolean;
 
-    private logo: HTMLImageElement;
+    private background: HTMLImageElement;
     private startButton: HTMLImageElement;
 
     public constructor(maxX: number, maxY: number) {
         super(maxX, maxY);
-        this.logo = CanvasRenderer.loadNewImage('./assets/final-background.png');
+        this.background = CanvasRenderer.loadNewImage('./assets/final-background.png');
         this.startButton = CanvasRenderer.loadNewImage('./assets/button.png');
         this.starting = false;
     }
@@ -39,8 +40,10 @@ export default class SceneStart extends Scene {
      * @returns the next scene to be rendered. null if no change
      */
     public getNextScene(): Scene | null {
-        // Add implementation here
-        return null;
+        // if (this.starting) {
+        //   return new Level(this.maxX, this.maxY);
+        // }
+          return null;
     }
 
     /**
@@ -51,9 +54,9 @@ export default class SceneStart extends Scene {
         CanvasRenderer.fillCanvas(canvas, '#e7cea2');
         CanvasRenderer.drawImage(
             canvas,
-            this.logo,
-            (canvas.width / 2) - (this.logo.width / 2),
-            (canvas.height / 2) - (this.logo.height / 2)
+            this.background,
+            (canvas.width / 2) - (this.background.width / 2),
+            (canvas.height / 2) - (this.background.height / 2)
         );
         CanvasRenderer.drawImage(
             canvas,
