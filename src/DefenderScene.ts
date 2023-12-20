0778e0f83ba55bad21d176ea1579e7ca116c75e9
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-12-20 11:30:18 +0100

    Added Player Movement (Working)

--- src/DefenderScene.ts
+++ src/DefenderScene.ts
@@ -3,13 +3,20 @@ import MouseListener from "./MouseListener.js";
 import Scene from "./Scene.js";
 import homeScene from "./homeScene.js";
 import winScene from "./winScene.js";
+import Player from "./Player.js";
 
 export default class DefenderScene extends Scene {
+
+  private keyMap: { [key: string]: boolean };
+  private currentDirection: string | null;
   private DefenderBackground: HTMLImageElement;
   private nextScene: Scene | null;
+  private player: Player;
 
+  // Amount of time the player has to complete the game in milliseconds
   private timeLimit: number = 300000;
 
+  // Function to calculate the time score
   private timeScoreMinutesandSeconds(): string {
     let minutes: number = Math.floor((this.timeLimit / (1000 * 60)) % 60);
     let seconds: number = Math.floor((this.timeLimit / 1000) % 60);
@@ -18,13 +25,48 @@ export default class DefenderScene extends Scene {
     return minutesString + ":" + secondsString;
   }
 
-
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
-    // Ff tijdelijke achtergrond voor de mail scene, deze moet nog worden aangepast
+
+    this.keyMap = {
+      ArrowLeft: false,
+      ArrowRight: false,
+      ArrowUp: false,
+      ArrowDown: false,
+    };
+    this.currentDirection = null;
 
     this.DefenderBackground = CanvasRenderer.loadNewImage("./assets/defender-background.png");
+    this.player = new Player(maxX / 2, maxY / 2, 100, 100, "./assets/player.png");
 
+    // Add event listener for keydown events
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    document.addEventListener('keyup', this.handleKeyUp.bind(this));
+  }
+
+  private handleKeyDown(event: KeyboardEvent): void {
+    if (this.keyMap.hasOwnProperty(event.key)) {
+      event.preventDefault();
+      this.keyMap[event.key] = true;
+      this.updateDirection();
+    }
+  }
+
+  private handleKeyUp(event: KeyboardEvent): void {
+    if (this.keyMap.hasOwnProperty(event.key)) {
+      event.preventDefault();
+      this.keyMap[event.key] = false;
+      this.updateDirection();
+    }
+  }
+
+  private updateDirection(): void {
+    const keys = Object.keys(this.keyMap).filter((key) => this.keyMap[key]);
+    if (keys.length === 0) {
+      this.currentDirection = null;
+    } else {
+      this.currentDirection = keys[0];
+    }
   }
 
   /**
@@ -37,6 +79,9 @@ export default class DefenderScene extends Scene {
     }
   }
 
+  /**
+   * Get the next scene to run
+   */
   public getNextScene(): Scene | null {
     if (this.timeLimit <= 0) {
       return new winScene(this.maxX, this.maxY);
@@ -49,12 +94,25 @@ export default class DefenderScene extends Scene {
    * @param elapsed elapsed ms since last update
    */
   public update(elapsed: number): void {
+
+    // Update the time limit
     if (this.timeLimit > 0) {
       this.timeLimit -= elapsed;
       console.log(this.timeLimit);
     } else {
       console.log("Defender scene ended");
       this.getNextScene();
+    }
+
+    // Update the player's position
+    if (this.currentDirection === 'ArrowLeft') {
+      this.player.moveLeft();
+    } else if (this.currentDirection === 'ArrowRight') {
+      this.player.moveRight();
+    } else if (this.currentDirection === 'ArrowUp') {
+      this.player.moveUp();
+    } else if (this.currentDirection === 'ArrowDown') {
+      this.player.moveDown();
     }
   }
 
@@ -65,11 +123,16 @@ export default class DefenderScene extends Scene {
   public render(canvas: HTMLCanvasElement): void {
     document.querySelectorAll("button").forEach((button) => {
       button.remove();
-    }); //alle buttons verwijderen van vorige pagina
+    });
+    // Render the background image
     document.body.style.backgroundImage = `url(${this.DefenderBackground.src})`;
+    const ctx = canvas.getContext("2d");
 
-    CanvasRenderer.writeText(canvas, "Defender Scene", canvas.width / 2, 60, "center", "Pixelated", 75, "White");
-    CanvasRenderer.writeText(canvas, this.timeScoreMinutesandSeconds(), canvas.width / 2, canvas.height * 0.05, "center", "Pixelated", 75, "White");
-
+    if (ctx) {
+      // Render the player on the canvas
+      this.player.render(canvas, ctx);
+      // Render the time score on the canvas
+      CanvasRenderer.writeText(canvas, this.timeScoreMinutesandSeconds(), canvas.width / 2, canvas.height * 0.05, "center", "Pixelated", 75, "White");
+    }
   }
 }
