7865c511a83d7c7e61dd7914acc9e756d7f99600
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2024-01-20 11:50:59 +0100

    Added Win/Lose Scene With Facts

--- src/scenes/loseScene.ts
+++ src/scenes/loseScene.ts
@@ -3,6 +3,26 @@ import MouseListener from "../MouseListener.js";
 import Scene from "../Scene.js";
 import homeScene from "./homeScene.js";
 
+const facts = [
+  "",
+  "",
+  "Did you know?",
+  "- Security cameras can be hacked if not properly secured.",
+  "- Phishing emails trick users into sharing info.",
+  "- Cybersecurity awareness is key to online safety.",
+];
+
+const paddingX = 20;
+const paddingY = 40;
+
+const textStyle = {
+  font: "12px Arial",
+  fillStyle: "White",
+  textAlign: "left" as CanvasTextAlign,
+  textBaseline: "top",
+};
+
+
 export default class winScene extends Scene {
   private loseBackground: HTMLImageElement;
   private clickNext: boolean = false;
@@ -10,7 +30,7 @@ export default class winScene extends Scene {
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     // Ff tijdelijke achtergrond voor de mail scene, deze moet nog worden aangepast
-    this.loseBackground = CanvasRenderer.loadNewImage("./assets/you-lose.png");
+    this.loseBackground = CanvasRenderer.loadNewImage("./assets/black-background.jpg");
   }
 
   /**
@@ -50,6 +70,13 @@ export default class winScene extends Scene {
       button.remove();
     }); //alle buttons verwijderen van vorige pagina
     document.body.style.backgroundImage = `url(${this.loseBackground.src})`;
-    CanvasRenderer.writeText(canvas, "Click to continue", canvas.width / 2, canvas.height / 1.5, "center", "Pixelated", 75, "White");
+    CanvasRenderer.writeText(canvas, "You Lost!", canvas.width / 2, canvas.height / 8, "center", "Pixelated", 75, "Red");
+    for (let i = 0; i < facts.length; i++) {
+      const x = paddingX;
+      const y = paddingY + i * 100; // ruimte/padding tussen de feiten
+
+      CanvasRenderer.writeText(canvas, facts[i], x, y, textStyle.textAlign, textStyle.font, null, textStyle.fillStyle);
+  }
+    CanvasRenderer.writeText(canvas, "Click to continue", canvas.width / 2, canvas.height - 30, "center", "Pixelated", 75, "Green");
   }
 }
